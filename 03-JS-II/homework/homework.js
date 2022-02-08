bf40581c7518e6b2113b326d028ebf537d3c985c
carlosguerra1272 <carlosguerra@gmail.com>
2022-02-08 10:22:20 -0500

    Tarea completada

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -59,7 +59,7 @@ function saludo(idioma) {
   } else if (idioma === "ingles") {
     return "Hello!"
   }
-  return
+  return "Hola!"
 }
 
 function colors(color) {
@@ -84,7 +84,7 @@ function colors(color) {
       return "This is orange"
       break
     default: 
-      return "This is blue"
+      return "Color not found"
       break 
   } 
 }
@@ -138,6 +138,7 @@ function fizzBuzz(numero) {
   } else if (numero % 5 === 0) {
     return "buzz"
   }
+  return numero
 }
 
 function operadoresLogicos(num1, num2, num3) {
@@ -147,15 +148,16 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
-  if (num1 === 0||num2 === 0||num3 === 0) {
-    return "Error"
-  } else if (num1 > num2 && num1 > num3 && num1 >0) {
-    return "Número 1 es mayor y positivo"
-  } else if (num1 < 0 || num2 < 0 || num3 <0 ) {
+    if (num1 === 0|| num2=== 0 || num3 ===0){
+      return "Error"
+    }
+    else if (num1 < 0 || num2 < 0 || num3 <0) {
     return "Hay negativos"
-  }else if (num3 > num2 && num3 > num1 ) {
-      var nuevo = num2 + 1 
-      return nuevo
+  } else if (num1 > num2 && num1 > num3) {
+    return "Número 1 es mayor y positivo"
+  } else if (num3 > num1 && num3 > num2){
+    var nuevo = num3 + 1
+    return nuevo
   } else {
     return false
   }
